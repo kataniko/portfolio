7304489ca7d7708b47caed056858021816c1bbe1
kataniko <tomasoliveira.31@gmail.com>
2023-10-30 00:54:46 +0000

    add

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-'use client'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from "./Header/header"
@@ -15,10 +15,7 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  const [play] = useSound("/night.mp3") 
-  useEffect(() => {
-    play();
-  }, [])
+ 
   return (
     <html lang="en">
       <body className={inter.className}>
