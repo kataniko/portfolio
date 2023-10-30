69d771c0fd70ad088301250d371f7ce9a6f0de6a
kataniko <tomasoliveira.31@gmail.com>
2023-10-30 00:56:19 +0000

    add

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from "./Header/header"
 import useSound from 'use-sound'
-import { useEffect } from 'react'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
